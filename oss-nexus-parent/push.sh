1295a0b02f168086c3605798c34dea7e45a10e98
gclm <1719982754@qq.com>
2020-09-21 22:14:40 +0800

    2020-09-21 22:14:40 发布新版本 [ERROR] No plugin found for prefix 'exec' in the current project and in the plugin groups [org.sonarsource.scanner.maven, org.apache.maven.plugins, org.codehaus.mojo] available from the repositories [local (/Users/gclm/.m2/repository), huawei_mirrors (http://localhost:8880/maven/public)] -> [Help 1] [ERROR] [ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch. [ERROR] Re-run Maven using the -X switch to enable full debug logging. [ERROR] [ERROR] For more information about the errors and possible solutions, please read the following articles: [ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/NoPluginFoundForPrefixException

--- oss-nexus-parent/push.sh
+++ oss-nexus-parent/push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
-#date=$(date "+%Y-%m-%d %H:%M:%S")
-#
-#echo "上传代码到 github"
-#git add .
-#git commit -m "$date 发布新版本 $version"
-#git push
+version=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+date=$(date "+%Y-%m-%d %H:%M:%S")
+
+echo "上传代码到 github"
+git add .
+git commit -m "$date 发布新版本 $version"
+git push
 
 echo "发布到 华为云"
 mvn clean deploy -P huawei-oss-release
